9c4fbb836784c0c979af655966635c5fc761225e
Felipe dos Santos <felipedossantosbezerra94@gmail.com>
2026-08-21 14:35:55 -0300

    subindo models.py e crud.py

---
+++ app/schemas.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel
+
+class ProdutoBase(BaseModel):
+  nome: str
+  descricao: str
+  preco: float
+  
+class ProdutoCreate(ProdutoBase):
+  pass
+
+class ProdutoResponse(ProdutoBase):
+  id: int
+  
+  class Config:
+    from_attributes = True
