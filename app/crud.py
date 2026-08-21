ad3180d8c5850498fdfb5ecf6016df9c56e55afe
Felipe dos Santos <felipedossantosbezerra94@gmail.com>
2026-08-21 14:05:36 -0300

    enviando models.py e crud.py

---
+++ app/crud.py
@@ -0,0 +1,3 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+from models import Produto
+from schema import ProdutoBase
---
+++ readme.md
@@ -0,0 +1,2 @@
+python3 -m venv venv --> usar para ativar as coisas
+.\venv\Script\activate 
