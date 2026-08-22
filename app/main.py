5863217f9fba3382a4cdc1783916118c93569174
Lucas-Rai78 <lucasrai.dev@gmail.com>
2026-08-22 19:57:07 -0300

    subindo frontend e o projeto finalizado

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Depends, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
 from typing import List
 
@@ -8,6 +9,14 @@ from app.database import engine, get_db
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Gerenciador de produtos")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.post("/produtos/", response_model=schemas.ProdutoResponse, status_code=201)
 def criar_produto(produto: schemas.ProdutoCreate, db: Session = Depends(get_db)):
--- readme.md
+++ readme.md
@@ -1,6 +1,19 @@
-python3 -m venv venv #ativa o ambiente virtual
-.\venv\Script\activate 
 
+### criar e ativar o ambiente virtual (venv)
+python3 -m venv venv
+#### windows
+.\venv\Scripts\activate
+#### linux
+source venv/bin/activate
+___
+### instalar as dependencias
+pip install -r requirements.txt
+___
+### executar o backend
 uvicorn app.main:app --reload #inicia o servidor de denvolvimento
-
+___
+### link da interface interativa
 http://127.0.0.1:8000/docs #swagger ui, interface interativa
+___
+### utilizar a extensão Live Server para rodar o frontend
+___
